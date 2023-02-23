45371b49ce0f7627795f458bf21adc0414c8fafa
N-Santiago <norbertosantiago82@gmail.com>
2023-02-22 20:41:45 -0600

    backend

---
+++ backend/src/main/java/goodsamaritan/beauty/BeautyApplication.java
@@ -0,0 +1,13 @@
+package goodsamaritan.beauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyApplication.class, args);
+	}
+
+}
