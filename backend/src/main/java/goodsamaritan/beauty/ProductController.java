615f7142c47cc96c9384f184fefd743b2cc6ab65
N-Santiago <norbertosantiago82@gmail.com>
2023-03-12 15:56:36 -0500

    merge

--- backend/src/main/java/goodsamaritan/beauty/ProductController.java
+++ backend/src/main/java/goodsamaritan/beauty/ProductController.java
@@ -1,18 +1,15 @@
 package goodsamaritan.beauty;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/products")
 public class ProductController {
     @Autowired
